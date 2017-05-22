15819fe7c84899e7a8057201d730a965776e0a15
ivan elimbi <ivan elimbi>
2017-05-22 19:10:58 +0200

    Lesson 10 premier commi

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -18,83 +18,110 @@ $("#header").append(formattedRole);
 
 var work = {
     jobs: [{
-            "employer": "Acensi",
-            "tittle": "Junior Developper",
-            "location": "Paris",
-            "dates": "2008-2010",
-            "description": ""
-        },
-        {
-            "employer": "Phirst Vanilla",
-            "tittle": "Developper",
-            "location": "Paris",
-            "dates": "2010-2014",
-            "description": "Leade developper on finance softaware project"
-        },
-        {
-            "employer": "Acensi",
-            "tittle": "Senior Developper",
-            "location": "Paris",
-            "dates": "2014-now",
-            "description": "Team Leader for a software team"
-        }
+        "employer": "Acensi",
+        "tittle": "Junior Developper",
+        "location": "Paris",
+        "dates": "2008-2010",
+        "description": "Junior for a front-office IT Team at an Asset Manager"
+    },
+    {
+        "employer": "Phirst Vanilla",
+        "tittle": "Developper",
+        "location": "Paris",
+        "dates": "2010-2014",
+        "description": "Lead developper for a front office IT team  at an Asset Manager"
+    },
+    {
+        "employer": "Acensi",
+        "tittle": "Senior Developper",
+        "location": "Paris",
+        "dates": "2014-now",
+        "description": "Team Leader for a software team"
+    }
     ]
 }
 
 var projects = [{
-            "title": "Dexia AM",
-            "Dates": "2008-2009",
-            "Description": "",
-        },
-        {
-            "title": "Natixis CIB",
-            "Dates": "2009-2011",
-            "Description": "",
-        },
-        {
-            "title": "AXA IM",
-            "Dates": "2011-2014",
-            "Description": "",
-        },
-        {
-            "title": "AXA IM",
-            "Dates": "2014-now",
-            "Description": "",
-        },
-    ],
+    "tittle": "Dexia AM",
+    "Dates": "2008-2009",
+    "Description": "A job",
+},
+{
+    "tittle": "Natixis CIB",
+    "Dates": "2009-2011",
+    "Description": "A job",
+},
+{
+    "tittle": "AXA IM",
+    "Dates": "2011-2014",
+    "Description": "A job",
+},
+{
+    "tittle": "AXA IM",
+    "Dates": "2014-now",
+    "Description": "A job",
+},
+];
 
-    var bio = {
-        "name": "Ivan Elimbi",
-        "role": "Developper",
-        "welcomeMessage": "",
-        "biopic": "",
-        "contacts": {
-            "mobile": "0675420049",
-            "email": "ivan.elimbi@acensi.fr",
-            "github": "ivanacensi",
-            "location": "Paris",
-        },
-        "skills": ["Web", ".NET", "SQL", "NoSQL", "Software Craftmanship", "Project Management"]
-    };
+var bio = {
+    "name": "Ivan Elimbi",
+    "role": "Developper",
+    "welcomeMessage": "",
+    "biopic": "",
+    "contacts": {
+        "mobile": "0675420049",
+        "email": "ivan.elimbi@acensi.fr",
+        "github": "ivanacensi",
+        "location": "Paris",
+    },
+    "skills": ["Web", ".NET", "SQL", "NoSQL", "Software Craftmanship", "Project Management"]
+};
 
 var education = {
     schools: [{
-            "name": "ECE",
-            "degre": "Master III",
-            "location": "Paris",
-            "majors ": ["Telecomunication and Network"],
-        },
-        {
-            "name": "Jeannne D'Albret",
-            "degre": "Prepa",
-            "location": "Saint-Germain en Laye",
-            "majors  ": ["Physique-Chimie"],
-        },
-        {
-            "name": "Paul Lapie",
-            "degre": "Lycée",
-            "location": "Courbevoie",
-            "majors ": ["Physique-Chimie"],
-        },
+        "name": "ECE",
+        "degre": "Master III",
+        "location": "Paris",
+        "majors ": ["Telecomunication and Network"],
+    },
+    {
+        "name": "Jeannne D'Albret",
+        "degre": "Prepa",
+        "location": "Saint-Germain en Laye",
+        "majors  ": ["Physique-Chimie"],
+    },
+    {
+        "name": "Paul Lapie",
+        "degre": "Lycée",
+        "location": "Courbevoie",
+        "majors ": ["Physique-Chimie"],
+    },
     ]
 }
+
+HTMLheaderName = HTMLheaderName.replace("%data%", bio.name);
+if (bio.skills.length > 0) {
+    $("#header").append(HTMLskillsStart);
+}
+
+function displayWork() {
+    for (job in work.jobs) {
+        $("#workExperience").append(HTMLworkStart);
+        var formatedHTMLworkEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formatedHTMLworkTitle = HTMLworkTitle.replace("%data%", work.jobs[job].tittle);
+        var line = formatedHTMLworkEmployer.concat(formatedHTMLworkTitle);
+        var formattedDate = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        var formattedLoacation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        $(".work-entry:last").append(line);
+        $(".work-entry:last").append(formattedDate);
+        $(".work-entry:last").append(formattedLoacation);
+        $(".work-entry:last").append(formattedDescription);
+    }
+};
+
+displayWork();
+
+$(document).click(function (loc) {
+    logClicks(loc.pageX, loc.pageY);
+}); 
